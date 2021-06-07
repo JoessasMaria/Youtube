90118b27b6ff73fbd941eb003233b87f7d036882
MathiasBuchwald <83806296+MathiasBuchwald@users.noreply.github.com>
2021-06-07 15:31:47 +0200

    Projektdateien hinzufügen.

---
+++ YoutubeConsole/Video.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeConsole
+{
+    class Video
+    {
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        public List<Comments> comments { get; set; }
+
+        public Video(string title, string description)
+        {
+            this.title = title;
+            this.description = description;
+        }
+    }
+}
