a68c7e68ca076bdd43ede046a946ddd00a70a913
JoessasMaria <83806296+MathiasBuchwald@users.noreply.github.com>
2021-06-14 16:07:55 +0200

    a

--- YoutubeConsole/Program.cs
+++ YoutubeConsole/Program.cs
@@ -15,6 +15,8 @@ namespace YoutubeConsole
                 try
                 {
                     int selection = Convert.ToInt32(Console.ReadLine());
+                    string title;
+                    string description;
                     switch (selection)
                     {
                         case 1:
@@ -29,7 +31,8 @@ namespace YoutubeConsole
                             break;
                         case 2:
                             Console.WriteLine("suchen");
-                            String title = Console.ReadLine();
+                            Console.WriteLine("Geben Sie den Titel ein, nachdem Sie suchen wollen:");
+                            title = Console.ReadLine();
                             List<Video> videos2 = dao.searchVideos(title);
 
                             foreach (Video video in videos2)
@@ -37,6 +40,17 @@ namespace YoutubeConsole
                                 Console.WriteLine(video.title + " - " + video.description);
                             }
 
+                            break;
+                        case 3:
+                            Console.WriteLine("einfügen");
+                            
+                            Console.WriteLine("Geben Sie den Titel des Videos ein:");
+                            title = Console.ReadLine();
+
+                            Console.WriteLine("Geben Sie die Beschreibung des Videos ein:");
+                            description = Console.ReadLine();
+
+                            dao.insertVideo(title, description);
                             break;
                         default:
                             break;
@@ -56,6 +70,7 @@ namespace YoutubeConsole
                 Console.WriteLine("menu");
                 Console.WriteLine("1 Alle anzeigen");
                 Console.WriteLine("2 Suchen");
+                Console.WriteLine("3 Einfügen");
                 Console.WriteLine("------------------");
             }
         }
